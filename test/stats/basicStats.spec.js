f549a9500fe8f8bb1139269640626ffb23f2e155
Barbara Wahl <wahl@hanover.edu>
2014-10-09 14:45:58 -0400

    Add tests for stats#sum. Close #23

--- test/stats/basicStats.spec.js
+++ test/stats/basicStats.spec.js
@@ -1,17 +1,19 @@
 var Variable  = require('../../panthrBase').Variable;
+var utils  = require('../../base/utils');
 var expect = require('chai').expect;
 var A = [
    new Variable([5.5, , 3.3, -2.5]),
    new Variable(['c', , 'x', 'x']),
    new Variable([true, , false, true], {mode: 'logical'}),
-   new Variable(['c', , 'x', 'x'], {mode: 'ord', levels:['x', 'c']}),
-   new Variable([])
+   new Variable(['c', , 'x', 'x'], {mode: 'ord', levels:['x', 'c']})
 ];
+var E = new Variable([]);
+
 describe('Basic statistics', function() {
    it('sum', function() {
       expect(Variable).to.respondTo('sum');
       A.forEach(function(v) {
-         expect(v.sum()).to.equal(null);
+         expect(utils.isMissing(v.sum())).to.be.true;
          if (v.length() > 0) { 
             expect(v.nonMissing().sum()).to.exist;
             expect(v.sum(true)).to.exist;
@@ -21,7 +23,8 @@ describe('Basic statistics', function() {
       expect(A[1].sum(true)).to.equal(1+2+2);
       expect(A[2].sum(true)).to.equal(1+0+1);
       expect(A[3].sum(true)).to.equal(2+1+1);
-      expect(A[4].sum(true)).to.equal(null);
+      expect(E.sum(true)).to.equal(0);
+      expect(E.sum()).to.equal(0);
    });
    it('mean', function() {
       expect(Variable).to.respondTo('mean');
