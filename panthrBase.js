64043832301be2fc96ef7da9dcc0e865aa765da4
Barbara Wahl <wahl@hanover.edu>
2016-07-17 10:43:41 -0400

    Expose Base.utils

--- panthrBase.js
+++ panthrBase.js
@@ -23,10 +23,12 @@ return function(loader) {
    Base.List = require('./base/list');
    /* Implementation of "statistics" datasets. */
    Base.Dataset = require('./base/dataset');
+   Base.utils = require('./base/utils');
 
    loader.addClass('Variable', Base.Variable);
    loader.addClass('List', Base.List);
    loader.addClass('Dataset', Base.Dataset);
+   loader.addClass('utils', Base.utils);
    loader.loadModule(require('./base/fun'));
    loader.loadModule(require('./base/base'));
    loader.loadModule(require('./base/stats'));
